e59ee23f017cc2fe64ef22786989acce1a259635
Jaden Monroe <jaden1@MacBook-Pro.lan>
2022-12-16 10:49:29 -0800

    created a component for the tables form

---
+++ front-end/src/tables/Tables.js
@@ -0,0 +1,14 @@
+import React, { useEffect,useState } from "react";
+
+function Tables(){
+    const [tables, setTables] = useState([])
+    
+
+
+
+
+
+    return <h1>nothing</h1>
+}
+
+export default Tables
