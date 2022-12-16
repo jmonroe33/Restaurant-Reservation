af15fb81210083f72a30685839e6647cd94caea3
Jaden Monroe <jaden1@MacBook-Pro.lan>
2022-12-16 15:06:00 -0800

    added in the create method on the /tables route

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -3,6 +3,7 @@ const router = require("express").Router()
 const MethodNotAllowed = require("../errors/MethodNotAllowed")
 const controller = require("./tables.controller")
 
+
 router.route('/')
     .get(controller.list)
     .post(controller.create)
