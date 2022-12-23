907e11c34c8e7365b2fead934b009c731ca18788
Jaden <jadenmonroe20001@outlook.com>
2022-12-22 19:54:26 -0800

    added in the needed service functions for the seat table functionality

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,17 +1,56 @@
 const knex = require("../db/connection")
 
 function list() {
-    return knex("tables").select("*").orderBy("table_name")
+    return knex("tables")
+        .select("*")
+        .orderBy("table_name")
 }
 
-function create(table){
+function create(table) {
     return knex("tables")
-    .insert(table)
-    .returning("*")
-    .then(createdRecord => createdRecord[0])
+        .insert(table)
+        .returning("*")
+        .then(createRecords => createRecords[0])
 }
+
+function read(table_id) {
+    return knex("tables")
+        .select("*")
+        .where({ table_id: table_id }).first()
+}
+
+function seatTable(reservation_id, table_id) {
+    return knex.transaction(function (trx) {
+        return knex("tables")
+            .where({ table_id: table_id })
+            .update({ reservation_id })
+            .returning("*")
+            .then((updatedTable) => updatedTable[0])
+            .then(() => {
+                return trx("reservations")
+                    .where({ reservation_id })
+                    .update({ status: "seated" });
+            });
+    });
+}
+function finishTable(table_id, reservation_id) {
+    return knex.transaction(function (trx) {
+      return trx("tables")
+        .where({ table_id: table_id })
+        .update({ reservation_id: null })
+        .then(() => {
+          return trx("reservations")
+            .where({ reservation_id })
+            .update({ status: "finished" });
+        });
+    });
+  }
 
 module.exports = {
     list,
-    create
+    create,
+    read,
+    seatTable,
+    finishTable,
+    
 }
