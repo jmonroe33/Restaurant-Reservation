83197997a679da3b9dfc2b73295c56cc61418f93
Jaden <jadenmonroe20001@outlook.com>
2022-12-22 21:07:58 -0800

    added in a create table component

---
+++ front-end/src/tables/CreateTable.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TableForm from "./TableForm";
+
+//create table
+export default function CreateTable() {
+  return (
+    <>
+      <h1>Create a new table</h1>
+      <TableForm />
+    </>
+  );
+}
