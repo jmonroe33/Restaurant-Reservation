0a87139bb35bf64c4c844ccbadac3a86fa705fee
Jaden <jadenmonroe20001@outlook.com>
2022-12-22 19:53:43 -0800

    added in the table_Id/seat route and its needed functions

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,13 +1,15 @@
-
-const router = require("express").Router()
-const MethodNotAllowed = require("../errors/MethodNotAllowed")
+const router = require("express").Router({ mergeParams: true })
+const methodNotAllowed = require("../errors/MethodNotAllowed")
 const controller = require("./tables.controller")
 
-
-router.route('/')
+router.route("/:table_id/seat")
+    .put(controller.seatTable)
+    .delete(controller.finishTable)
+    .all(methodNotAllowed)
+    
+router.route("/")
     .get(controller.list)
     .post(controller.create)
-    .all(MethodNotAllowed)
-
+    .all(methodNotAllowed)
 
 module.exports = router
