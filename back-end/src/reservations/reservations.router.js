585de960456053584697838b89f334845227adf1
Jaden <jadenmonroe20001@outlook.com>
2022-12-22 19:47:48 -0800

    added in the needed routes to complete reservations router

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -7,12 +7,32 @@ const methodNotAllowed = require("../errors/MethodNotAllowed")
 const router = require("express").Router();
 const controller = require("./reservations.controller");
 
-router.route("/:reservation_id")
-    .get(controller.read)
-    .all(methodNotAllowed)
+//edit
+router
+  .route("/:reservation_id/edit")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
-router.route("/")
-    .get(controller.list)
-    .post(controller.create)
-    .all(methodNotAllowed)
+  //status
+router
+  .route("/:reservation_id/status")
+  .get(controller.read)
+  .put(controller.updateStatus)
+  .all(methodNotAllowed);
+
+  //reservation_id
+router
+  .route("/:reservation_id")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
+  
+//home
+  router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
 module.exports = router;
